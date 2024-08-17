b649457e7f947f997d9c12e4d68a4f0723925d00
Tbars84 <camoilo.tabares.84@gmail.com>
2024-08-17 14:20:24 -0500

    Cambio a ambiente modular + sidebar funcional

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
+
+  //{ path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+  //{ path: 'procedimientos-pacientes', loadChildren: () => import('./modules/procedimientos-pacientes/procedimientos-pacientes.module').then(m => m.ProcedimientosPacientesModule) },
+  //{ path: 'ingreso-insumos', loadChildren: () => import('./modules/ingreso-insumos/ingreso-insumos.module').then(m => m.IngresoInsumosModule) },
+  //{ path: 'procedimientos-disponibles', loadChildren: () => import('./modules/procedimientos-disponibles/procedimientos-disponibles.module').then(m => m.ProcedimientosDisponiblesModule) },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
